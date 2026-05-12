c75d42735337082179f62a6100d0f97ad02fc3e2
AxiomLC <axiom@loftcoin.net>
2026-05-12 11:31:00 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,10 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/fred')) {
+      url.pathname = url.pathname.replace('/fred', '') || '/';
+      return env.ASSETS.fetch(new Request(url.toString(), request));
+    }
     return env.ASSETS.fetch(request);
   }
 };
